ee1e838d26d15e4c9cbef6cbd895d803d193655d
AngelCer3 <aectangel@hotmail.com>
2022-06-15 20:54:54 -0500

    Ejemplo2

---
+++ views/ejemplo1.php
@@ -0,0 +1,47 @@
+<div class="container">
+  <div class="row">
+        <div class="col-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body text-primary">
+                    <h5 class="card-title">Creacion de Numeros de Control</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body text-black">
+                    <h5 class="card-title">Creacion de Alumnos  <i class="fa-solid fa-user-ninja"></i></h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body text-black">
+                    <h5 class="card-title">Listado de Alumnos</h5>
+                </div>
+            </div>
+        </div>
+          <div class="col-4">
+            <div class="card border-danger mb-4">
+                <div class="card-body text-danger">
+                    <h5 class="card-title">Pendiente</h5>
+                </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body text-black">
+                    <h5 class="card-title">Pendiente</h5>
+                </div>
+            </div>
+        </div>
+          <div class="col-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body text-black">
+                    <h5 class="card-title">Pendiente</h5>
+                </div>
+            </div>
+         </div>
+      </div>
+    </div>
+</div>
